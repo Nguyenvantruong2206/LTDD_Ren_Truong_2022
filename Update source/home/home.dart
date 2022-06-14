d1d1dde3a1f4993a85971c197b91825fc815b5cd
truong <bokrinak@gmail.com>
2022-06-14 10:51:10 +0700

    truong update source

---
+++ Update source/home/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("INFORMATION TECHNOLOGY CAO THẮNG"),
+      ),
+    );
+  }
+}
---
+++ Update source/main.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home/home.dart';
+import 'package:flutter_application_1/login/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Login(),
+    );
+  }
+}
