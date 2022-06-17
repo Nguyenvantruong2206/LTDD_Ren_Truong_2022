6f4c58d93b350c01d570639a6c03813bc620c010
truong <bokrinak@gmail.com>
2022-06-17 22:15:12 +0700

    truong up tim kiem

---
+++ Tìm kiếm/navigatorbar.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login/login.dart';
+import 'package:flutter_application_1/profiles/profiles.dart';
+import 'package:flutter_application_1/thongbao/thongbao.dart';
+import 'package:flutter_application_1/trangchu/home.dart';
+
+class navigatorbar extends StatefulWidget {
+  @override
+  _navigatorbarState createState() => _navigatorbarState();
+}
+
+class _navigatorbarState extends State<navigatorbar> {
+  int _selectIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: CustomSearch());
+              })
+        ],
+      ),
+      body: IndexedStack(
+        children: [homepage(), Thong_bao_page(), profiles()],
+        index: _selectIndex,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Trang chủ"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notification_add), label: "Thông báo"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: "Trang cá nhân"),
+        ],
+        backgroundColor: Colors.blueAccent,
+        selectedItemColor: Colors.amber,
+        currentIndex: _selectIndex,
+        onTap: (index) {
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Admin"),
+              accountEmail: Text("admin@caothang.edu.vn"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Phòng CTCTHSSV"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Phòng Đào Tạo"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Đoàn Thanh Niên"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Khoa CNTT"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text("Học Bổng"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.replay),
+              title: Text("Đăng xuất"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Login();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSearch extends SearchDelegate {
+  List<String> allData = [
+    'Phòng đào tạo',
+    'Đoàn thanh niên',
+    'Phòng hành chính-quản trị',
+    'Phòng CTCT-HSSV',
+    'Khoa CNTT',
+    'Học Bổng'
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
