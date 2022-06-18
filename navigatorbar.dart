f4c0a2b19979622692265a55e5ae3c0308c31fc2
truong <bokrinak@gmail.com>
2022-06-18 23:03:13 +0700

    truong update tim kiem

--- navigatorbar.dart
+++ navigatorbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login/login.dart';
 import 'package:flutter_application_1/profiles/profiles.dart';
+import 'package:flutter_application_1/profilespage/profilespage.dart';
 import 'package:flutter_application_1/thongbao/thongbao.dart';
 import 'package:flutter_application_1/trangchu/home.dart';
 
@@ -9,124 +10,145 @@ class navigatorbar extends StatefulWidget {
   _navigatorbarState createState() => _navigatorbarState();
 }
 
+bool _iconbool = false;
+IconData _iconlight = Icons.wb_sunny;
+IconData _icondart = Icons.nights_stay;
+ThemeData _lighttheme =
+    ThemeData(primarySwatch: Colors.amber, brightness: Brightness.light);
+ThemeData _darttheme = ThemeData(
+  primarySwatch: Colors.red,
+  brightness: Brightness.dark,
+);
+
 class _navigatorbarState extends State<navigatorbar> {
   int _selectIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
-        actions: [
-          IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {
-                showSearch(context: context, delegate: CustomSearch());
-              })
-        ],
-      ),
-      body: IndexedStack(
-        children: [homepage(), Thong_bao_page(), Profile(), ProfilePage()],
-        index: _selectIndex,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Trang chủ",
-            backgroundColor: Colors.blueAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notification_add),
-            label: "Thông báo",
-            backgroundColor: Colors.blueAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "Trang cá nhân",
-            backgroundColor: Colors.blueAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Cài Đặt",
-            backgroundColor: Colors.blueAccent,
-          ),
-        ],
-        selectedItemColor: Colors.amber,
-        currentIndex: _selectIndex,
-        onTap: (index) {
-          setState(() {
-            _selectIndex = index;
-          });
-        },
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Admin"),
-              accountEmail: Text("admin@caothang.edu.vn"),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Text(
-                  "A",
-                  style: TextStyle(fontSize: 40.0),
-                ),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: _iconbool ? _darttheme : _lighttheme,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(""),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _iconbool = !_iconbool;
+                  });
+                },
+                icon: Icon(_iconbool ? _icondart : _iconlight),
               ),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text("Phòng CTCTHSSV"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text("Phòng Đào Tạo"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text("Đoàn Thanh Niên"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text("Khoa CNTT"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text("Học Bổng"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.replay),
-              title: Text("Đăng xuất"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Login();
-                    },
+              IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    showSearch(context: context, delegate: CustomSearch());
+                  }),
+            ],
+          ),
+          body: IndexedStack(
+            children: [homepage(), Thong_bao_page(), Profile(), ProfilePage()],
+            index: _selectIndex,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Trang chủ",
+                backgroundColor: Colors.blueAccent,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.notification_add),
+                label: "Thông báo",
+                backgroundColor: Colors.blueAccent,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: "Trang cá nhân",
+                backgroundColor: Colors.blueAccent,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: "Cài Đặt",
+                backgroundColor: Colors.blueAccent,
+              ),
+            ],
+            selectedItemColor: Colors.amber,
+            currentIndex: _selectIndex,
+            onTap: (index) {
+              setState(() {
+                _selectIndex = index;
+              });
+            },
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("Admin"),
+                  accountEmail: Text("admin@caothang.edu.vn"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      "A",
+                      style: TextStyle(fontSize: 40.0),
+                    ),
                   ),
-                );
-              },
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Phòng CTCTHSSV"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Phòng Đào Tạo"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Đoàn Thanh Niên"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Khoa CNTT"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Học Bổng"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.replay),
+                  title: Text("Đăng xuất"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return Login();
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
 
