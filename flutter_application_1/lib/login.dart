4f03dce1dc1ad49504275bd2d7b803499addda2e
NguyenVanSuRen-CDTH19PMC <surennguyen01@gmail.com>
2022-06-20 21:45:40 +0700

    ren update file login

--- flutter_application_1/lib/login.dart
+++ flutter_application_1/lib/login.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/forgotpassword.dart';
-import 'package:flutter_application_1/homepage.dart';
-import 'package:flutter_application_1/signup.dart';
+import 'package:flutter_application_3/forgotpassword.dart';
+import 'package:flutter_application_3/home.dart';
+import 'package:flutter_application_3/homepage.dart';
+import 'package:flutter_application_3/signup.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -19,148 +20,150 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-        constraints: BoxConstraints.expand(),
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-              child: Container(
-                width: 70,
-                height: 70,
-                padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, color: Color(0xffd8d8d8)),
-                child: Image.asset("assets/images/1.png"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Container(
+                  width: 70,
+                  height: 70,
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xffd8d8d8)),
+                  child: Image.asset("assets/images/1.png"),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
-              child: Text(
-                "Hello\nWelcome To\nInformation Technology CKC",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    fontSize: 25),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+                child: Text(
+                  "Hello\nWelcome To\nInformation Technology CKC",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 25),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: TextField(
-                style: TextStyle(fontSize: 18, color: Colors.black),
-                controller: _user,
-                decoration: InputDecoration(
-                  labelText: "EMAIL",
-                  errorText: _userinvalid
-                      ? _usererror
-                      : null, //nghĩa là ko thông báo lỗi
-                  labelStyle: TextStyle(
-                    color: Color(0xff888888),
-                    fontSize: 17,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  controller: _user,
+                  decoration: InputDecoration(
+                    labelText: "EMAIL",
+                    errorText: _userinvalid
+                        ? _usererror
+                        : null, //nghĩa là ko thông báo lỗi
+                    labelStyle: TextStyle(
+                      color: Color(0xff888888),
+                      fontSize: 17,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-              child: Stack(
-                alignment: AlignmentDirectional
-                    .centerEnd, // cho chữ show nằm cùng hàng vs password
-                children: <Widget>[
-                  TextField(
-                    style: TextStyle(fontSize: 18, color: Colors.black),
-                    controller: _pass,
-                    obscureText:
-                        !_showpassword, // mật khẩu sẽ hiện dấu chấm tròn
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      errorText: _passinvaild ? _passerror : null,
-                      labelStyle: TextStyle(
-                        color: Color(0xff888888),
-                        fontSize: 17,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Stack(
+                  alignment: AlignmentDirectional
+                      .centerEnd, // cho chữ show nằm cùng hàng vs password
+                  children: <Widget>[
+                    TextField(
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                      controller: _pass,
+                      obscureText:
+                          !_showpassword, // mật khẩu sẽ hiện dấu chấm tròn
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        errorText: _passinvaild ? _passerror : null,
+                        labelStyle: TextStyle(
+                          color: Color(0xff888888),
+                          fontSize: 17,
+                        ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: showpass,
+                    GestureDetector(
+                      onTap: showpass,
+                      child: Text(
+                        _showpassword
+                            ? "HIDE"
+                            : "SHOW", // nếu đang showpass sẽ hiện text là hide còn ko thì là show
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(29),
+                      ),
+                    ),
+                    onPressed: onsignclicked,
                     child: Text(
-                      _showpassword
-                          ? "HIDE"
-                          : "SHOW", // nếu đang showpass sẽ hiện text là hide còn ko thì là show
-                      style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold),
+                      "SIGN IN",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-              child: SizedBox(
+              Container(
+                height: 30,
                 width: double.infinity,
-                height: 56,
-                child: RaisedButton(
-                  color: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(29),
-                    ),
-                  ),
-                  onPressed: onsignclicked,
-                  child: Text(
-                    "SIGN IN",
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 30,
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => RegisterPage()),
-                      );
-                    },
-                    child: Text(
-                      "NEW USER? SIGN UP",
-                      style: TextStyle(
-                        fontSize: 13,
-                        color: Color(0xff888888),
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => quenmatkhau()));
-                    },
-                    child: Text(
-                      "FORGOT PASSWORD",
-                      style: TextStyle(
-                        fontSize: 13,
-                        color: Colors.blue,
+                              builder: (BuildContext context) =>
+                                  RegisterPage()),
+                        );
+                      },
+                      child: Text(
+                        "NEW USER? SIGN UP",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Color(0xff888888),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => quenmatkhau()),
+                        );
+                      },
+                      child: Text(
+                        "FORGOT PASSWORD",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
